aa322d811514083d9a49aef57b620e7546cc9401
Robby Ranshous <rranshous@gmail.com>
2014-10-03 11:36:39 -0400

    Storing / returning headers, timestamp; fixed encoding

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 require 'sinatra/sequel'
 require 'json'
+require 'pry'
 
 migration "create graffiti table" do
   database.create_table :graffitis do
     primary_key :id
     text        :body
     text        :url
+    text        :headers
     timestamp   :created_at
 
     index :url, :unique => false
@@ -14,24 +16,36 @@ migration "create graffiti table" do
 end
 
 class Graffiti < Sequel::Model
-  def to_json
-    JSON.dump(
-      body: body
-    )
+  def to_h
+    { body: body,
+      headers: headers,
+      timestamp: created_at.to_i }
   end
+end
+
+get '/' do
+  Graffiti.all.map(&:to_h).to_json
 end
 
 post '/:url' do
   request.body.rewind
   body = request.body.read
+  headers = request_headers.to_json
   url = params[:url]
-  g = Graffiti.new(url: url, body: body, created_at: Time.now)
+  g = Graffiti.new(url: url, headers: headers, body: body, created_at: Time.now)
   g.save
-  g.to_json
+  g.to_h.to_json
 end
 
 get '/:url' do
   content_type :json
   url = params[:url]
-  Graffiti.filter(:url => url).all.map(&:to_json).to_json
+  Graffiti.filter(:url => url).all.map(&:to_h).to_json
+end
+
+
+helpers do
+  def request_headers
+    env.inject({}){|acc, (k,v)| acc[$1.upcase] = v if k =~ /^http_(.*)/i; acc}
+  end
 end
